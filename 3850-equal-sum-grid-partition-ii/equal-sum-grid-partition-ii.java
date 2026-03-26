7a304afadf0b7f8f802397d653c097fc9cb0209d
Aditya Pratap <adityapratap8957@gmail.com>
2026-03-26 20:54:25 +0530

    Time: 236 ms (82.35%) | Memory: 240.6 MB (88.24%) - LeetSync

---
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
@@ -0,0 +1,67 @@
+class Solution {
+    long total = 0;
+    public boolean checkHorCuts(int[][] grid){
+        Set<Long> st = new HashSet<>();
+        int m = grid.length;
+        int n = grid[0].length;
+        long top = 0;
+        for(int i=0; i<m-1; i++){
+            for(int j =0; j<n; j++){
+                top += grid[i][j];
+                st.add((long)grid[i][j]);
+            }
+            long bottom = total - top;
+            long diff = top-bottom;
+
+            if(diff == 0) return true;
+            if(diff == grid[0][0] || diff == grid[0][n-1]) return true;
+            if(diff == grid[i][0]) return true;
+            if(i>0 && n>1 && st.contains(diff)) return true; 
+
+        }
+        return false;
+    }
+    public boolean canPartitionGrid(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+            }
+        }
+        if (checkHorCuts(grid)) return true;
+
+        reverse(grid);
+
+        if (checkHorCuts(grid)) return true;
+
+        reverse(grid);
+
+        int[][] transposeGrid = new int[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                transposeGrid[j][i] = grid[i][j];
+            }
+        }
+
+        if (checkHorCuts(transposeGrid)) return true;
+
+        reverse(transposeGrid);
+
+        return checkHorCuts(transposeGrid);
+    }
+
+    public void reverse(int[][] grid){
+        int top = 0, bottom = grid.length - 1;
+
+        while(top < bottom){
+            int[] temp = grid[top];
+            grid[top] = grid[bottom];
+            grid[bottom] = temp;
+
+            top++;
+            bottom--;
+        }
+    }
+}
