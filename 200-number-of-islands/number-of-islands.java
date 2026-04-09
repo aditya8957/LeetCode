6af0e00534a63d4fa62ac8c3b59dd16345795eea
Aditya Pratap <adityapratap8957@gmail.com>
2026-04-09 11:37:57 +0530

    Time: 7 ms (13.91%) | Memory: 51.3 MB (93.64%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,23 +1,55 @@
 class Solution {
-    public void dfs(char[][] grid, int row, int col){
-        if(row<0 || col<0 || row>=grid.length || col>= grid[0].length ||
-         grid[row][col] != '1') return;
+    class Pair {
+        int first;
+        int sec;
 
+        Pair(int first, int sec) {
+            this.first = first;
+            this.sec = sec;
+        }
+    }
+
+    public void bfs(char[][] grid, int row, int col){
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(row,col));
         grid[row][col] = '0';
 
-        dfs(grid,row-1,col);
-        dfs(grid,row+1,col);
-        dfs(grid,row,col-1);
-        dfs(grid,row,col+1);
+        while(!q.isEmpty()){
+            int r = q.peek().first;
+            int c= q.peek().sec;
+            q.remove();
+
+            for(int drow=-1; drow<=1; drow++){
+                for(int dcol=-1; dcol<=1; dcol++){
+
+                    if(Math.abs(drow) == Math.abs(dcol)) continue;
+
+                    int nrow = r+drow;
+                    int ncol = c+dcol;
+
+                    if(nrow>=0 && ncol>=0 && nrow<grid.length && ncol<grid[0].length &&
+                    grid[nrow][ncol] == '1'){
+
+                        if(grid[nrow][ncol] == '1'){
+                            q.add(new Pair(nrow, ncol));
+                            grid[nrow][ncol] = '0';
+                        }
+                    }
+                    
+                }
+            }
+        }
 
     }
+
     public int numIslands(char[][] grid) {
-        int count =0;
-        for(int i=0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j] == '1'){
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid,i,j);
+                    bfs(grid, i, j);
                 }
             }
         }
